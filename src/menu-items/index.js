632b8528d97fb97ac09c7467d04de68c985261ff
dovietthang <thangdv062@gmail.com>
2023-11-16 11:31:04 +0700

    main

---
+++ src/menu-items/index.js
@@ -0,0 +1,13 @@
+// import dashboard from './dashboard';
+// import pages from './pages';
+// import utilities from './utilities';
+// import other from './other';
+import menuItem from './menuItem';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+  items: [menuItem]
+};
+
+export default menuItems;
